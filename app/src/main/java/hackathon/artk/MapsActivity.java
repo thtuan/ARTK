ddc66bd4ac2788838c289cd251a1a225540a6f9c
thtuan <tuan.ho@gcalls.co>
2017-11-25 13:38:29 +0700

    update map

---
+++ app/src/main/java/hackathon/artk/MapsActivity.java
@@ -0,0 +1,69 @@
+package hackathon.artk;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+  private GoogleMap mMap;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_maps);
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
+  }
+
+
+  /**
+   * Manipulates the map once available.
+   * This callback is triggered when the map is ready to be used.
+   * This is where we can add markers or lines, add listeners or move the camera. In this case,
+   * we just add a marker near Sydney, Australia.
+   * If Google Play services is not installed on the device, the user will be prompted to install
+   * it inside the SupportMapFragment. This method will only be triggered once the user has
+   * installed Google Play services and returned to the app.
+   */
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    mMap = googleMap;
+
+
+    // circle settings
+    int radiusM = 150; // your radius in meters
+    double latitude = -34;// your center latitude
+    double longitude = 151;// your center longitude
+
+    // draw circle
+    int d = 500; // diameter
+    Bitmap bm = Bitmap.createBitmap(d, d, Config.ARGB_8888);
+    Canvas c = new Canvas(bm);
+    Paint p = new Paint();
+    p.setColor(getResources().getColor(android.R.color.holo_green_light));
+    c.drawCircle(d/2, d/2, d/2, p);
+
+    // generate BitmapDescriptor from circle Bitmap
+    BitmapDescriptor bmD = BitmapDescriptorFactory.fromBitmap(bm);
+
+    // Add a marker in Sydney and move the camera
+    LatLng sydney = new LatLng(-34, 151);
+    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in "
+        + "Sydney").icon(bmD));
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+  }
+}
